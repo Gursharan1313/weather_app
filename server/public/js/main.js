6c3991840a8112348097ea569c8beaaa585cf15d
Gursharan Singh <gursharan.dfr@gmail.com>
2022-01-19 00:08:34 +0530

    almost complete weather app

--- server/public/js/main.js
+++ server/public/js/main.js
@@ -9,8 +9,30 @@ console.log('test js')
 // })
 
 
-fetch('http://localhost:3000/weather?address=!').then((response) => {
-    response.json().then((data) => {
-        console.log(data)
+
+
+
+const weatherForm = document.querySelector('form')
+const formData = document.querySelector('input')
+const pOne = document.querySelector('#p1')
+const pTwo = document.querySelector('#p2')
+
+pOne.textContent = 'Please Add Location in form'
+
+
+weatherForm.addEventListener('submit',(e) => {
+    e.preventDefault();
+    const data = formData.value
+    fetch('http://localhost:3000/weather?address='+data).then((response) => {
+        response.json().then((data) => {
+           if(data.Rerror){
+            pOne.textContent = data.Rerror 
+           }
+           else{
+               console.log(data)
+            pOne.textContent = data.Location
+            pTwo.textContent = 'Temp: '+data.ForData.currentTemp+' Feel Like: '+data.ForData.feelLike+' Description: '+data.ForData.description
+           }
+        })
     })
 })
